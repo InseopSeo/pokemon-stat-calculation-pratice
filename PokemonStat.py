a6920e621d743c11226c552ebf5913185df416d4
Inseop Seo <seo000408@gmail.com>
2023-03-16 12:23:03 +0900

    make flag to be understood easily

--- PokemonStat.py
+++ PokemonStat.py
@@ -1,38 +1,38 @@
 import math
 
-flag2 = False
-flag3 = False
+flag_a = False
+flag_effort = False
 
-while(not flag2):
+while(not flag_a):
     try:
         a = int(input("종족값:"))
         if (a > 0 and a <= 300):
-            flag2 = True
+            flag_a = True
         else:
             print("1과 300 사이의 값을 입력하세요.")
 
     except:
         print("유효하지 않은 값입니다.")
 
-while(not flag3):
+while(not flag_effort):
     try:
         effort = int(input("노력치:"))
         if effort >= 0 and effort <=252:
-            flag3 = True
+            flag_effort = True
         else:
             print("0과 252 사이의 값을 입력하세요.")
     except:
         print("유효하지 않은 값입니다.")
         
 stat = (a * 2 + 31 + effort/4) * 0.5 + 5
-flag = False
-while(not flag):
+flag_ch = False
+while(not flag_ch):
     character = input("성격 보정 여부(y or n):")
     if(character == 'n' or character == 'N'):
-        flag = True
+        flag_ch = True
         print("성격 보정 안함")
     elif (character == 'y' or character == 'Y'):
-        flag = True
+        flag_ch = True
         print("성격 보정 함")
         stat = stat * 1.1
     else:
